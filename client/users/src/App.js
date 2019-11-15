498dbaf4659e220b269199a1fbef4ea1ced9c82b
Rashmi <poddar.rashmi@gmail.com>
2019-11-15 13:56:49 -0500

    Working on the react app

--- client/users/src/App.js
+++ client/users/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Route } from 'react-router-dom';
+
+import Register from './components/Register';
+import Login from './components/Login';
+
 function App() {
   return (
     <div>
       <h1>Testing</h1>
+      <Route path='/register' component={Register} />
+      {/* <Route path='login' component={Login} /> */}
     </div>
   );
 }
